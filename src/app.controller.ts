e0df2298c5d78096f58b7f8447bee797471613e8
alfonsoulloao <alfonsoulloa.o@gmail.com>
2023-12-15 23:17:33 -0300

    se crean archivos de conf y dockerfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('default')
-@Controller('default')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'http://localhost:3000/';
+    return 'swagger se encuantra en http://localhost:3000/api';
   }
 }
